4ece5716f2f665977afee055b14b930bccc9ee40
Matt Simpson <recurse.then.reverse@gmail.com>
2020-07-31 03:10:02 -0500

    refactored to retain single connection.  memory and performance

--- reader_with_config.py
+++ reader_with_config.py
@@ -6,33 +6,66 @@ import sys
 import getopt
 import pika
 import json
+import signal
 
-type_application = "application"
-type_application_json = "application/json"
+class squid_to_rmq_injector:
 
-type_img = ["image/gif",
-            "image/png",
-            "image/jpeg",
-            "image/webp",]
+    type_application = "application"
+    type_application_json = "application/json"
 
-type_media =["video/mp4",
-             "video/mpeg"]
+    type_img = ["image/gif",
+                "image/png",
+                "image/jpeg",
+                "image/webp",]
 
-advert_url_keys = ['daca_images', 'googlesyndication']
+    type_media =["video/mp4",
+                "video/mpeg"]
 
-def read_log(file_name, rabbit_mq_host, queue_name, tail_log, read_full_log, clear_log_on_finish):
-    # Set the filename and open the file
-    if read_full_log:
-        read_whole_log(file_name, rabbit_mq_host, queue_name)
-        if clear_log_on_finish:
-            clear_the_log_file(file_name)
+    advert_url_keys = ['daca_images', 'googlesyndication']
 
-    if tail_log:
-        print("Open file: ", file_name)
-        file = open(file_name, 'r')
+    def __init__(self, config: dict):
+        
+        self._file_name = config['file_name']
+        self._rabbit_mq_host = config['host']
+        self._queue_name = config['queue_name']
+        self._tail_log = config['tail_file']
+        self._read_full_log = config['read_full_log']
+        self._clear_log_on_finish = config['clear_log_on_finish'] 
+        self._config = config
+
+        self._rmq_connection = self.init_rabbit_mq_connection()
+        
+    def init_rabbit_mq_connection(self):
+        credentials = pika.PlainCredentials('rabbit', 'rabbit') # user, password
+        parameters = pika.ConnectionParameters(self._rabbit_mq_host, 5672, '/', credentials)
+        return pika.BlockingConnection(parameters)
+
+    def send_msg(self, msg):
+        channel = self._rmq_connection.channel()
+        channel.queue_declare(queue=self._queue_name , durable=True,
+                            arguments={'x-message-ttl': 86400000, 'x-max-length': 5242880})
+
+        channel.basic_publish(exchange='',
+                            routing_key=self._queue_name,
+                            body=msg)
+
+    def start(self):
+        # Set the filename and open the file
+        if self._read_full_log:
+            self.read_whole_log()
+
+            if self._clear_log_on_finish:
+                self.clear_the_log_file()
+
+        if self._tail_log:
+            self.tail_log()
+
+    def tail_log(self):
+        print("Open file: ", self._file_name)
+        file = open(self._file_name, 'r')
 
         # Find the size of the file and move to the end
-        st_results = os.stat(file_name)
+        st_results = os.stat(self._file_name)
         st_size = st_results[6]
         file.seek(st_size)
         print("Initial File Size: ", str(st_size))
@@ -41,58 +74,43 @@ def read_log(file_name, rabbit_mq_host, queue_name, tail_log, read_full_log, cle
             where = file.tell()
             line = file.readline()
             if not line:
-                time.sleep(1)
+                time.sleep(5)
                 file.seek(where)
             else:
-                if is_image(line if line else None):
-                    print(line if line else None)  # already has newline
-                    send_msg(line if line else 'NONE', rabbit_mq_host, queue_name)
+                if self.is_message_type(line if line else None, "image"):
+                    print("Push: ", line if line else None)  
+                    self.send_msg(line if line else 'NONE')
 
-# this is verbose, I know
-def is_image(log_line):
-    if log_line is None:
-        return False            
-    log_line = str(log_line)  # only cast once
-    parts = log_line.split("|||") # only split once
-    if len(parts) > 4: 
-        if  parts[4].strip() in type_img: #trim as needed
-            return True
-    return False 
-
-
-def read_whole_log(file_name, rabbit_mq_host, queue_name):
-    print("sending log lines to queue")
-    with open(file_name, 'r') as f:
-        for line in f:
-            send_msg(line, rabbit_mq_host, queue_name)
-
-def clear_the_log_file(file_name):
-    print("Clearing log file for read")
-    with open(file_name, 'w'):
-        pass
+    def is_message_type(self, log_line, message_string):
+        if log_line is None:
+            return False            
+        log_line = str(log_line)  # only cast once
+        parts = log_line.split("|||") # only split once
+        if len(parts) > 4: 
+            if message_string in parts[4]:
+                return True
+        return False 
 
 
-def send_msg(msg, host, queue_name):
-    credentials = pika.PlainCredentials('rabbit', 'rabbit') # user, password
-    parameters = pika.ConnectionParameters(host, 5672, '/', credentials)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name, durable=True,
-                          arguments={'x-message-ttl': 86400000, 'x-max-length': 5242880})
+    def read_whole_log(self):
+        print("sending log lines to queue")
+        with open(self._file_name, 'r') as f:
+            for line in f:
+                self.send_msg(line)
 
-    channel.basic_publish(exchange='',
-                          routing_key=queue_name,
-                          body=msg)
+    def clear_the_log_file(self):
+        print("Clearing log file for read")
+        with open(self._file_name, 'w'):
+            pass
+
+
+
 
 
 if __name__ == "__main__":
     with open('reader_config.json', 'r') as f:
         config = json.load(f)
-        print(config)
-        read_log(config['file_name'], 
-                    config['host'], 
-                    config['queue_name'], 
-                    config['tail_file'], 
-                    config['read_full_log'],
-                    config['clear_log_on_finish'] )
+        stri = squid_to_rmq_injector(config)
+        stri.start()
+
         
